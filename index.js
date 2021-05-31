b12baf88070a69bd123f83ed7a0dd127e2aad7bd
David Ochoa <radikal_david@hotmail.com>
2021-05-31 10:57:38 -0500

    Añadidos nuevos enlaces a proyectos

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
 //Index - List of android projects
 var androidProjects = new Vue({
-  el: '#android-projects',
-  data: {
-    androidProjects: [
-      { name: 'DashrX', id: '1', url: 'window.location.href="apps/index-dashrx.html"', thumb: 'android-dashrx.png' },
-      { name: 'Checa Tu Peso', id: '2', url: 'window.location.href="apps/index-imc.html"', thumb: 'android-imc.png' },
-      { name: 'Código embolia', id: '3', url: 'window.location.href="apps/index-ictus.html"', thumb: 'android-ictus.png' },
-      { name: 'Lactancia educativa', id: '4', url: 'window.location.href="apps/index-lactapp.html"', thumb: 'android-lactancia.png' },
-      { name: 'Encuentro estatal de investigación', id: '5', url: 'window.location.href="apps/index-eei.html"', thumb: 'android-EEI.png' },
-      { name: 'Alerta COVID19', id: '6', url: 'window.location.href="apps/index-alertacovid19.html"', thumb: 'android-alertacovid19.png' },
-      { name: 'Registro de entrega', id: '7', url: 'window.location.href="apps/index-registrodeentrega.html"', thumb: 'android-registrodeentrega.png' },
-      { name: 'Registro Vacunación Influenza', id: '8', url: 'window.location.href="apps/index-ssnlvacinflu.html"', thumb: 'android-reg-vacinflu.png' },
-    ],
-  },
+    el: '#android-projects',
+    data: {
+        androidProjects: [
+            { name: 'DashrX', id: '1', url: 'window.location.href="apps/index-dashrx.html"', thumb: 'android-dashrx.png' },
+            { name: 'Checa Tu Peso', id: '2', url: 'window.location.href="apps/index-imc.html"', thumb: 'android-imc.png' },
+            { name: 'Código embolia', id: '3', url: 'window.location.href="apps/index-ictus.html"', thumb: 'android-ictus.png' },
+            { name: 'Lactancia educativa', id: '4', url: 'window.location.href="apps/index-lactapp.html"', thumb: 'android-lactancia.png' },
+            { name: 'Encuentro estatal de investigación', id: '5', url: 'window.location.href="apps/index-eei.html"', thumb: 'android-EEI.png' },
+            { name: 'Alerta COVID19', id: '6', url: 'window.location.href="apps/index-alertacovid19.html"', thumb: 'android-alertacovid19.png' },
+            { name: 'Registro de entrega', id: '7', url: 'window.location.href="apps/index-registrodeentrega.html"', thumb: 'android-registrodeentrega.png' },
+            { name: 'Registro Vacunación Influenza', id: '8', url: 'window.location.href="apps/index-ssnlvacinflu.html"', thumb: 'android-reg-vacinflu.png' },
+        ],
+    },
 });
 
 //Index - List of iOS projects
 var iOSProjects = new Vue({
-  el: '#iOS-projects',
-  data: {
-    iOSProjects: [
-      { name: 'Checa Tu Peso', id: '1', url: 'window.location.href="apps/index-imc.html"', thumb: 'android-imc.png' },
-      { name: 'Código embolia', id: '2', url: 'window.location.href="apps/index-ictus.html"', thumb: 'android-ictus.png' },
-      { name: 'Lactancia educativa', id: '3', url: 'window.location.href="apps/index-lactapp.html"', thumb: 'android-lactancia.png' },
-      { name: 'Alerta COVID19', id: '4', url: 'window.location.href="apps/index-alertacovid19.html"', thumb: 'ios-alertacovid19.png' },
-    ],
-  },
+    el: '#iOS-projects',
+    data: {
+        iOSProjects: [
+            { name: 'Checa Tu Peso', id: '1', url: 'window.location.href="apps/index-imc.html"', thumb: 'android-imc.png' },
+            { name: 'Código embolia', id: '2', url: 'window.location.href="apps/index-ictus.html"', thumb: 'android-ictus.png' },
+            { name: 'Lactancia educativa', id: '3', url: 'window.location.href="apps/index-lactapp.html"', thumb: 'android-lactancia.png' },
+            { name: 'Alerta COVID19', id: '4', url: 'window.location.href="apps/index-alertacovid19.html"', thumb: 'ios-alertacovid19.png' },
+        ],
+    },
 });
 
 //Index - List of web projects
 var webProjects = new Vue({
-  el: '#web-projects',
-  data: {
-    webProjects: [
-      { name: 'Apoyos', id: '1', url: 'window.location.href="webs/index-.html"', thumb: 'ssnl-general.png' },
-      { name: 'Nóminas', id: '2', url: 'window.location.href="webs/index-.html"', thumb: 'ssnl-general.png' },
-      { name: 'Gafetes', id: '3', url: 'window.location.href="webs/index-.html"', thumb: 'ssnl-general.png' },
-      { name: 'LDRP', id: '4', url: 'window.location.href="webs/index-.html"', thumb: 'ssnl-general.png' },
-      { name: 'Seguimiento de trámites', id: '5', url:'window.location.href="webs/index-tramites.html"', thumb: 'ssnl-general.png' },
-      { name: 'Asenco', id: '4', id: '6', url: 'window.location.href="webs/index-asenco.html"', thumb: 'web-asenco.png' },
-      { name: 'Registro Vacunación Influenza', id: '7', url:'window.location.href="webs/index-registro-vac-influenza.html"', thumb: 'ssnl-general.png' },
-      { name: 'RED Cancer', id: '8', url:'window.location.href="webs/index-red-cancer.html"', thumb: 'ssnl-general.png' }
-    ],
-  },
+    el: '#web-projects',
+    data: {
+        webProjects: [
+            { name: 'Apoyos', id: '1', url: 'window.location.href="webs/index-apoyos.html"', thumb: 'ssnl-general.png' },
+            { name: 'Nóminas', id: '2', url: 'window.location.href="webs/index-.html"', thumb: 'ssnl-general.png' },
+            { name: 'Gafetes', id: '3', url: 'window.location.href="webs/index-.html"', thumb: 'ssnl-general.png' },
+            { name: 'LDRP', id: '4', url: 'window.location.href="webs/index-ldrp.html"', thumb: 'ssnl-general.png' },
+            { name: 'Seguimiento de trámites', id: '5', url: 'window.location.href="webs/index-tramites.html"', thumb: 'ssnl-general.png' },
+            { name: 'Asenco', id: '4', id: '6', url: 'window.location.href="webs/index-asenco.html"', thumb: 'web-asenco.png' },
+            { name: 'Registro Vacunación Influenza', id: '7', url: 'window.location.href="webs/index-registro-vac-influenza.html"', thumb: 'ssnl-general.png' },
+            { name: 'RED Cancer', id: '8', url: 'window.location.href="webs/index-red-cancer.html"', thumb: 'ssnl-general.png' }
+        ],
+    },
 });
--- webs/data.js
+++ webs/data.js
@@ -1,80 +1,126 @@
 var tramites = new Vue({
-  el: '#data-tramites',
-  data: {
-    title: 'Atención y Seguimiento de Trámites',
-    summary: 'Sistema para la Subsecretaría de Regulación y Fomento Sanitario de la Secretaría de Salud de Nuevo León',
-    appType: 'Web',
-    tech:[
-      { tecno: 'HTML' },
-      { tecno: 'PHP' },
-      { tecno: 'Yii2 framework' }
-    ],
-    carousel:[
-      { img: 'web-tramites-1.png' },
-      { img: 'web-tramites-2.png' },
-      { img: 'web-tramites-3.png' },
-      { img: 'web-tramites-4.png' }
-    ],
-  },
+    el: '#data-tramites',
+    data: {
+        title: 'Atención y Seguimiento de Trámites',
+        summary: 'Sistema para la Subsecretaría de Regulación y Fomento Sanitario de la Secretaría de Salud de Nuevo León, donde se ingresan los trámites correspondientes al area para control y asignación, también se proporciona una forma en la que la persona involucrada puede darle seguimiento a su trámite.',
+        link: 'http://saludnl.gob.mx/regulacion-sanitaria/tramites',
+        appType: 'Web',
+        tech: [
+            { tecno: 'HTML' },
+            { tecno: 'PHP' },
+            { tecno: 'Yii2 framework' }
+        ],
+        carousel: [
+            { img: 'web-tramites-1.png' },
+            { img: 'web-tramites-2.png' },
+            { img: 'web-tramites-3.png' },
+            { img: 'web-tramites-4.png' },
+            { img: 'web-tramites-5.png' },
+            { img: 'web-tramites-6.png' },
+        ],
+    },
 });
 
 var asenco = new Vue({
-  el: '#data-asenco',
-  data: {
-    title: 'ASENCO Integram S.A. de C.V.',
-    summary: 'Página web para la consultora ASENCO Integram S.A. de C.V.  http://www.asenco.mx',
-    appType: 'Web',
-    tech:[
-      { tecno: 'Wordpress' }
-    ],
-    carousel:[
-      { img: 'web-asenco-1.png' }
-    ],
-  },
+    el: '#data-asenco',
+    data: {
+        title: 'ASENCO Integram S.A. de C.V.',
+        summary: 'Página web para la consultora ASENCO Integram S.A. de C.V.',
+        link: ' http://www.asenco.mx',
+        appType: 'Web',
+        tech: [
+            { tecno: 'Wordpress' }
+        ],
+        carousel: [
+            { img: 'web-asenco-1.png' },
+            { img: 'web-asenco-2.png' },
+            { img: 'web-asenco-3.png' },
+        ],
+    },
 });
 
 
 var registroVacInfluenza = new Vue({
-  el: '#data-registro-vacuna-influenza',
-  data: {
-    title: 'Sistema de registro para vacunación contra la influenza',
-    summary: 'Sistema para la Secretaría de Salud de Nuevo León para llevar un' +
-              'registro de las personas a vacunar y tener fluidez en su aplicación' +
-              'en el modulo drive thru. Se acompaña de una app para el personal médico. http://saludnl.gob.mx/vacunacion/influenza',
-    appType: 'Web',
-    tech:[
-      { tecno: 'HTML' },
-      { tecno: 'PHP' },
-      { tecno: 'MySQL' },
-      { tecno: 'Yii2 Framework' },
-      { tecno: 'Sweet Alert 2' }
-    ],
-    carousel:[
-      { img: 'reg_vac_influenza_1.png' },
-      { img: 'reg_vac_influenza_2.png' },
-      { img: 'reg_vac_influenza_3.png' },
-      { img: 'reg_vac_influenza_4.png' },
-      { img: 'reg_vac_influenza_5.png' },
-      { img: 'reg_vac_influenza_6.png' },
-    ],
-  },
+    el: '#data-registro-vacuna-influenza',
+    data: {
+        title: 'Sistema de registro para vacunación contra la influenza',
+        summary: 'Sistema para la Secretaría de Salud de Nuevo León para llevar un' +
+            'registro de las personas a vacunar y tener fluidez en su aplicación' +
+            'en el modulo drive thru. Se acompaña de una app para el personal médico. http://saludnl.gob.mx/vacunacion/influenza',
+        appType: 'Web',
+        tech: [
+            { tecno: 'HTML' },
+            { tecno: 'PHP' },
+            { tecno: 'MySQL' },
+            { tecno: 'Yii2 Framework' },
+            { tecno: 'Sweet Alert 2' }
+        ],
+        carousel: [
+            { img: 'reg_vac_influenza_1.png' },
+            { img: 'reg_vac_influenza_2.png' },
+            { img: 'reg_vac_influenza_3.png' },
+            { img: 'reg_vac_influenza_4.png' },
+            { img: 'reg_vac_influenza_5.png' },
+            { img: 'reg_vac_influenza_6.png' },
+        ],
+    },
 });
 
 
 var redCancer = new Vue({
-  el: '#data-red-cancer',
-  data: {
-    title: 'Sistema RED Cancer',
-    summary: 'Sistema para captura de los casos de cancer en el estado de Nuevo León',
-    appType: 'Web',
-    tech:[
-      { tecno: 'PHP' },
-      { tecno: 'Yii2' },
-      { tecno: 'Bootstrap' },
-      { tecno: 'JavaScript' },
-    ],
-    carousel:[
-      { img: 'red-cancer-1.png' }
-    ],
-  },
+    el: '#data-red-cancer',
+    data: {
+        title: 'Sistema RED Cancer',
+        summary: 'Sistema para captura de los casos de cancer en el estado de Nuevo León',
+        appType: 'Web',
+        tech: [
+            { tecno: 'PHP' },
+            { tecno: 'Yii2' },
+            { tecno: 'Bootstrap' },
+            { tecno: 'JavaScript' },
+        ],
+        carousel: [
+            { img: 'red-cancer-1.png' }
+        ],
+    },
+});
+
+var apoyos = new Vue({
+    el: '#data-apoyos',
+    data: {
+        title: 'Apoyos',
+        summary: 'Sistema para captura de los apoyos otorgados por el departamento de Gestoría y Atención Ciudadana de la Secretaría de Salud de Nuevo León',
+        appType: 'Web',
+        tech: [
+            { tecno: 'PHP' },
+            { tecno: 'Yii2' },
+            { tecno: 'Bootstrap' },
+            { tecno: 'JavaScript' },
+        ],
+        carousel: [
+            { img: 'web-apoyos-1.png' },
+            { img: 'web-apoyos-2.png' },
+            { img: 'web-apoyos-3.png' },
+        ],
+    },
+});
+
+var ldrp = new Vue({
+    el: '#data-ldrp',
+    data: {
+        title: 'Sistema LDRP',
+        summary: 'Sistema para captura de licencias sanitarias, reconocimientos y permisos sanitarios, así como el distintivo salud que otroga la Subsecretaría de Regulación y Fomento Sanitario de la Secretaría de Salud de Nuevo León.',
+        appType: 'Web',
+        tech: [
+            { tecno: 'PHP' },
+            { tecno: 'Yii2' },
+            { tecno: 'Bootstrap' },
+            { tecno: 'JavaScript' },
+        ],
+        carousel: [
+            { img: 'web-ldrp-1.png' },
+            { img: 'web-ldrp-2.png' },
+            { img: 'web-ldrp-3.png' },
+        ],
+    },
 });
