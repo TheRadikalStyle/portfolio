4c1b1577947f106b30f92ee105fdc01cff54aac6
David Ochoa <radikal_david@hotmail.com>
2021-09-09 15:27:51 -0500

    UI changed, own theme

---
+++ dist/portfolio.js
@@ -0,0 +1,26 @@
+$(function(){
+
+    $('btn').click(function(){
+        //var destination = this.dataset.destination;
+        //renderPage(destination)
+        console.log(this);
+        return false; //Disable reload
+    });
+
+});
+
+function renderPage(pageToRender){
+    //$("#content").load(pageToRender);
+    $.get(pageToRender, function( data ) {
+        $( "#content" ).html( data );
+        console.log( "Load was performed." );
+      })
+      .done(function(){
+          console.log("DONE");
+      })
+      .fail(function(){
+          console.log("FAIL");
+      });
+
+      return false;
+}
--- index.js
+++ index.js
@@ -3,14 +3,14 @@ var androidProjects = new Vue({
     el: '#android-projects',
     data: {
         androidProjects: [
-            { name: 'DashrX', id: '1', url: 'window.location.href="apps/index-dashrx.html"', thumb: 'android-dashrx.png' },
-            { name: 'Checa Tu Peso', id: '2', url: 'window.location.href="apps/index-imc.html"', thumb: 'android-imc.png' },
-            { name: 'Código embolia', id: '3', url: 'window.location.href="apps/index-ictus.html"', thumb: 'android-ictus.png' },
-            { name: 'Lactancia educativa', id: '4', url: 'window.location.href="apps/index-lactapp.html"', thumb: 'android-lactancia.png' },
-            { name: 'Encuentro estatal de investigación', id: '5', url: 'window.location.href="apps/index-eei.html"', thumb: 'android-EEI.png' },
-            { name: 'Alerta COVID19', id: '6', url: 'window.location.href="apps/index-alertacovid19.html"', thumb: 'android-alertacovid19.png' },
-            { name: 'Registro de entrega', id: '7', url: 'window.location.href="apps/index-registrodeentrega.html"', thumb: 'android-registrodeentrega.png' },
-            { name: 'Registro Vacunación Influenza', id: '8', url: 'window.location.href="apps/index-ssnlvacinflu.html"', thumb: 'android-reg-vacinflu.png' },
+            { name: 'DashrX', id: '1', url: 'renderPage("apps/index-dashrx.html")', thumb: 'android-dashrx.png' },
+            { name: 'Checa Tu Peso', id: '2', url: 'renderPage("apps/index-imc.html")', thumb: 'android-imc.png' },
+            { name: 'Código embolia', id: '3', url: 'renderPage("apps/index-ictus.html")', thumb: 'android-ictus.png' },
+            { name: 'Lactancia educativa', id: '4', url: 'renderPage("apps/index-lactapp.html")', thumb: 'android-lactancia.png' },
+            { name: 'Encuentro estatal de investigación', id: '5', url: 'renderPage("apps/index-eei.html")', thumb: 'android-EEI.png' },
+            { name: 'Alerta COVID19', id: '6', url: 'renderPage("apps/index-alertacovid19.html")', thumb: 'android-alertacovid19.png' },
+            { name: 'Registro de entrega', id: '7', url: 'renderPage("apps/index-registrodeentrega.html")', thumb: 'android-registrodeentrega.png' },
+            { name: 'Registro Vacunación Influenza', id: '8', url: 'renderPage("apps/index-ssnlvacinflu.html")', thumb: 'android-reg-vacinflu.png' },
         ],
     },
 });
@@ -20,10 +20,10 @@ var iOSProjects = new Vue({
     el: '#iOS-projects',
     data: {
         iOSProjects: [
-            { name: 'Checa Tu Peso', id: '1', url: 'window.location.href="apps/index-imc.html"', thumb: 'android-imc.png' },
-            { name: 'Código embolia', id: '2', url: 'window.location.href="apps/index-ictus.html"', thumb: 'android-ictus.png' },
-            { name: 'Lactancia educativa', id: '3', url: 'window.location.href="apps/index-lactapp.html"', thumb: 'android-lactancia.png' },
-            { name: 'Alerta COVID19', id: '4', url: 'window.location.href="apps/index-alertacovid19.html"', thumb: 'ios-alertacovid19.png' },
+            { name: 'Checa Tu Peso', id: '1', url: 'renderPage("apps/index-imc.html")', thumb: 'android-imc.png' },
+            { name: 'Código embolia', id: '2', url: 'renderPage("apps/index-ictus.html")', thumb: 'android-ictus.png' },
+            { name: 'Lactancia educativa', id: '3', url: 'renderPage("apps/index-lactapp.html")', thumb: 'android-lactancia.png' },
+            { name: 'Alerta COVID19', id: '4', url: 'renderPage("apps/index-alertacovid19.html")', thumb: 'ios-alertacovid19.png' },
         ],
     },
 });
@@ -33,15 +33,15 @@ var webProjects = new Vue({
     el: '#web-projects',
     data: {
         webProjects: [
-            { name: 'Apoyos', id: '1', url: 'window.location.href="webs/index-apoyos.html"', thumb: 'ssnl-general.png' },
-            { name: 'Nóminas', id: '2', url: 'window.location.href="webs/index-.html"', thumb: 'ssnl-general.png' },
-            { name: 'Gafetes', id: '3', url: 'window.location.href="webs/index-.html"', thumb: 'ssnl-general.png' },
-            { name: 'LDRP', id: '4', url: 'window.location.href="webs/index-ldrp.html"', thumb: 'ssnl-general.png' },
-            { name: 'Seguimiento de trámites', id: '5', url: 'window.location.href="webs/index-tramites.html"', thumb: 'ssnl-general.png' },
-            { name: 'Asenco', id: '4', id: '6', url: 'window.location.href="webs/index-asenco.html"', thumb: 'web-asenco.png' },
-            { name: 'Registro Vacunación Influenza', id: '7', url: 'window.location.href="webs/index-registro-vac-influenza.html"', thumb: 'ssnl-general.png' },
-            { name: 'RED Cancer', id: '8', url: 'window.location.href="webs/index-red-cancer.html"', thumb: 'ssnl-general.png' },
-            { name: 'Registro Vacunación Personal de Salud', id: '9', url: 'window.location.href="webs/index-reg-covid19-persalud.html"', thumb: 'ssnl-general.png' }
+            { name: 'Apoyos', id: '1', url: 'renderPage("webs/index-apoyos.html")', thumb: 'ssnl-general.png' },
+            { name: 'Nóminas', id: '2', url: 'renderPage("webs/index-.html")', thumb: 'ssnl-general.png' },
+            { name: 'Gafetes', id: '3', url: 'renderPage("webs/index-.html")', thumb: 'ssnl-general.png' },
+            { name: 'LDRP', id: '4', url: 'renderPage("webs/index-ldrp.html")', thumb: 'ssnl-general.png' },
+            { name: 'Seguimiento de trámites', id: '5', url: 'renderPage("webs/index-tramites.html")', thumb: 'ssnl-general.png' },
+            { name: 'Asenco', id: '4', id: '6', url: 'renderPage("webs/index-asenco.html")', thumb: 'web-asenco.png' },
+            { name: 'Registro Vacunación Influenza', id: '7', url: 'renderPage("webs/index-registro-vac-influenza.html")', thumb: 'ssnl-general.png' },
+            { name: 'RED Cancer', id: '8', url: 'renderPage("webs/index-red-cancer.html")', thumb: 'ssnl-general.png' },
+            { name: 'Registro Vacunación Personal de Salud', id: '9', url: 'renderPage("webs/index-reg-covid19-persalud.html")', thumb: 'ssnl-general.png' }
         ],
     },
 });
